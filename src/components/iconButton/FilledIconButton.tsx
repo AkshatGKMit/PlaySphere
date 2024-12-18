084cac66f5b4e7dc155ced364059547796d679ef
AkshatGKMit <akshat@gkmit.co>
2024-12-18 16:33:56 +0530

    feat: Add icon button components

---
+++ src/components/iconButton/FilledIconButton.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+import { ICON_BUTTON_CONSTANTS } from '@constants';
+import { useAppSelector } from '@store';
+
+import IconButtonMain from './IconButtonMain';
+
+const { CONTAINER_COLOR, ICON_COLOR } = ICON_BUTTON_CONSTANTS.FILLED.THEME;
+
+const FilledIconButton = (props: IconButtonProps) => {
+  const theme = useAppSelector((state) => state.theme.colors);
+
+  return (
+    <IconButtonMain
+      backgroundColor={theme.all[CONTAINER_COLOR]}
+      color={theme.all[ICON_COLOR]}
+      {...props}
+    />
+  );
+};
+
+export default FilledIconButton;
---
+++ src/components/iconButton/OutlinedIconButton.tsx
@@ -0,0 +1,41 @@
+import React from 'react';
+
+import { ICON_BUTTON_CONSTANTS } from '@constants';
+import { useAppSelector } from '@store';
+import { colorWithOpacity } from '@utility/themeHelpers';
+
+import IconButtonMain from './IconButtonMain';
+
+const { THEME, MEASUREMENTS } = ICON_BUTTON_CONSTANTS.OUTLINED;
+
+const { OUTLINE_WIDTH } = MEASUREMENTS;
+
+const {
+  CONTAINER_COLOR,
+  ICON_COLOR,
+  OUTLINE_COLOR,
+  DISABLED_OUTLINE_COLOR,
+  DISABLED_OUTLINE_OPACITY,
+} = THEME;
+
+const OutlinedIconButton = (props: IconButtonProps) => {
+  const theme = useAppSelector((state) => state.theme.colors);
+
+  const { disabled } = props;
+
+  const outlineColor = disabled
+    ? colorWithOpacity(theme.all[DISABLED_OUTLINE_COLOR], DISABLED_OUTLINE_OPACITY)
+    : theme.all[OUTLINE_COLOR];
+
+  return (
+    <IconButtonMain
+      backgroundColor={theme.all[CONTAINER_COLOR]}
+      color={theme.all[ICON_COLOR]}
+      borderColor={outlineColor}
+      borderWidth={OUTLINE_WIDTH}
+      {...props}
+    />
+  );
+};
+
+export default OutlinedIconButton;
