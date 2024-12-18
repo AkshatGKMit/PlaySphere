143b137e02f365470ff7c26ba3933f08b260a748
AkshatGKMit <akshat@gkmit.co>
2024-12-18 12:07:34 +0530

    feat: Add path configs

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,35 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['.'],
+        alias: {
+          '@assets': './src/assets',
+          '@components': './src/components',
+          '@config': './src/config',
+          '@constants': './src/constants',
+          '@navigation': './src/navigation',
+          '@network': './src/network',
+          '@screens': './src/screens',
+          '@store': './src/store',
+          '@themes': './src/themes',
+          '@utility': './src/utility',
+        },
+      },
+    ],
+    [
+      'module:react-native-dotenv',
+      {
+        envName: 'API',
+        moduleName: '@env',
+        path: '.env',
+        safe: false,
+        allowUndefined: true,
+        verbose: false,
+      },
+    ],
+    'react-native-reanimated/plugin',
+  ],
 };
