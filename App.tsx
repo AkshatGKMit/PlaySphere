4cae7c6681d930b62853642994c5eef5edf741d4
AkshatGKMit <akshat@gkmit.co>
2024-12-18 15:01:34 +0530

    feat: Add redux store

--- App.tsx
+++ App.tsx
@@ -1,9 +1,28 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React, { useEffect } from 'react';
+import { View, Text, useColorScheme } from 'react-native';
+import { Provider } from 'react-redux';
 
 import { APP_NAME } from '@constants';
+import store, { useAppDispatch } from '@store';
+import { switchTheme } from '@store/reducers/theme';
+import { ThemeMode } from '@themes';
 
 const App = () => {
+  return (
+    <Provider store={store}>
+      <Main />
+    </Provider>
+  );
+};
+
+const Main = () => {
+  const colorScheme = useColorScheme();
+  const dispatch = useAppDispatch();
+
+  useEffect(() => {
+    dispatch(switchTheme(colorScheme ?? ThemeMode.light));
+  }, [colorScheme, dispatch]);
+
   return (
     <View>
       <Text>{APP_NAME}</Text>
