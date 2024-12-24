10b980cca1597e3825bcd7feba4cb4f79ad16f22
AkshatGKMit <akshat@gkmit.co>
2024-12-24 13:08:11 +0530

    feat: Add user details query

---
+++ src/constants/query.ts
@@ -0,0 +1,14 @@
+export const QueryKeys = {
+  mainGameList: 'main games',
+  authentication: 'authentication',
+  currentUser: 'current user',
+  userDetails: 'user details',
+  userCollections: 'user collections',
+  gameInCollection: 'game in collection',
+  createNewCollection: 'create new collection',
+  removeCollection: 'remove collection',
+  updateGameInCollection: 'update game in collection',
+  myCollections: 'myCollections',
+  collectionGames: 'collectionGames',
+  removeGameFromCollection: 'removeGameInCollection',
+};
