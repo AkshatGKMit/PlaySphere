3e0355df53642f9797ccc63c152f9a4b6bb961e8
AkshatGKMit <akshat@gkmit.co>
2024-12-24 14:24:55 +0530

    fix: Fix network types

--- src/@types/network.d.ts
+++ src/@types/network.d.ts
@@ -1,6 +1,18 @@
-import { AxiosRequestConfig } from 'axios';
+import { AxiosError, AxiosRequestConfig, AxiosResponse } from 'axios';
 
 declare global {
+  interface ApiSuccessResponse<T> {
+    success: true;
+    result: AxiosResponse<T>;
+  }
+
+  interface ApiErrorResponse<T> {
+    success: false;
+    error: AxiosError<T>;
+  }
+
+  type ApiResponse<Success, Error> = Promise<ApiSuccessResponse<Success> | ApiErrorResponse<Error>>;
+
   type ApiCallError = { code: number | string; message: string };
 
   interface ApiCallConfig<Params = {}> extends AxiosRequestConfig {
