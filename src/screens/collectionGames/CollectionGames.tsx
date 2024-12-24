280a4d272b28fffef14d187b9067958f6952b64d
AkshatGKMit <akshat@gkmit.co>
2024-12-24 13:19:36 +0530

    feat: Add collection games screen

---
+++ src/screens/collectionGames/CollectionGames.tsx
@@ -0,0 +1,157 @@
+import React, { useCallback, useEffect, useMemo, useState } from 'react';
+import { Alert, FlatList, NativeScrollEvent, NativeSyntheticEvent, View } from 'react-native';
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
+import { RouteProp, useNavigation, useRoute } from '@react-navigation/native';
+
+import FeedGameCard from '@components/feedGameCard';
+import { IconButton } from '@components/iconButton';
+import Loader from '@components/loader';
+import { ListEmptyComponent, ListFooterComponent } from '@components/pageListComponent';
+import TextBlock from '@components/textBlock';
+import useStyles from '@config/useStyles';
+import { Elevation, FontWeight, Icons, Typography } from '@constants';
+import useCollectionQuery from '@network/hooks/useCollectionQuery';
+import useCollectionFeedsQuery from '@network/hooks/useCollectionFeedsQuery';
+import useCollectionMutation from '@network/hooks/useCollectionMutation';
+import { useAppSelector } from '@store';
+import { globalStyles } from '@themes';
+import { getShadowStyle } from '@utility/style';
+
+import ThemedStyles from './styles';
+
+const CollectionGames = () => {
+  const { goBack } = useNavigation<StackNavigation>();
+  const { params } = useRoute<RouteProp<RootStackParamList>>();
+
+  const insets = useSafeAreaInsets();
+  const { colors: theme } = useAppSelector((state) => state.theme);
+
+  const [yOffset, setYOffset] = useState(0);
+
+  const { collectionId, collectionName } = params! as CollectionGamesRouteProps;
+
+  const {
+    collectionFeeds = [],
+    online,
+    fetchNextPage,
+    isFetchingNextPage,
+    hasNextPage,
+  } = useCollectionFeedsQuery(collectionId);
+
+  const { removeCollectionLoading, mutateRemoveCollection, removeCollectionSuccess } =
+    useCollectionMutation();
+
+  const { refetch } = useCollectionQuery();
+
+  const closeScreen = useCallback(() => {
+    refetch();
+    goBack();
+  }, [goBack, refetch]);
+
+  const onPressDelete = useCallback(() => {
+    Alert.alert('Confirm Exit', 'Are you sure you want to exit?', [
+      {
+        text: 'Cancel',
+        style: 'cancel',
+      },
+      {
+        text: 'Delete',
+        onPress: () => {
+          mutateRemoveCollection(collectionId);
+        },
+      },
+    ]);
+  }, [collectionId, mutateRemoveCollection]);
+
+  useEffect(() => {
+    if (removeCollectionSuccess) {
+      closeScreen();
+    }
+  }, [closeScreen, removeCollectionSuccess]);
+
+  const onEndReached = () => {
+    if (online.isConnected && !isFetchingNextPage) {
+      fetchNextPage();
+    }
+  };
+
+  const onScroll = useCallback(({ nativeEvent }: NativeSyntheticEvent<NativeScrollEvent>) => {
+    const { y } = nativeEvent.contentOffset;
+
+    setYOffset(y);
+  }, []);
+
+  const styles = useStyles((themeColors) => ThemedStyles(themeColors, insets));
+
+  const headerStyles = useMemo(
+    () => [
+      styles.header,
+      { backgroundColor: yOffset > 0 ? theme.all.surfaceContainer : theme.all.surface },
+      getShadowStyle(yOffset > 0 ? Elevation.level5 : Elevation.level0),
+    ],
+    [styles.header, theme.all.surface, theme.all.surfaceContainer, yOffset],
+  );
+
+  return (
+    <View style={styles.screen}>
+      <View style={headerStyles}>
+        <IconButton
+          icon={Icons.materialCommunityIcons.arrowLeft}
+          onPress={closeScreen}
+        />
+        <TextBlock
+          typography={Typography.headlineLarge}
+          fontWeight={FontWeight.heavy}
+          style={globalStyles.flex1}
+        >
+          {collectionName}
+        </TextBlock>
+        {removeCollectionLoading ? (
+          <Loader size={'small'} />
+        ) : (
+          <IconButton
+            icon={Icons.materialCommunityIcons.trashCan}
+            onPress={onPressDelete}
+          />
+        )}
+      </View>
+      <FlatList
+        numColumns={2}
+        style={globalStyles.flex1}
+        contentContainerStyle={styles.listContentStyle}
+        columnWrapperStyle={styles.columnStyles}
+        data={collectionFeeds}
+        renderItem={({ item }) => {
+          const { id } = item;
+
+          return (
+            <FeedGameCard
+              key={id}
+              feed={item}
+              collectionId={collectionId}
+            />
+          );
+        }}
+        keyExtractor={({ id }) => id.toString()}
+        onScroll={onScroll}
+        ListEmptyComponent={
+          <ListEmptyComponent
+            styles={styles}
+            theme={theme}
+          />
+        }
+        ListFooterComponent={
+          <ListFooterComponent
+            hasData={collectionFeeds.length !== 0}
+            hasNextPage={hasNextPage}
+            showNoConnectionScreenMessage={online.showNoConnectionScreenMessage}
+          />
+        }
+        showsVerticalScrollIndicator={false}
+        onEndReached={onEndReached}
+      />
+    </View>
+  );
+};
+
+export default CollectionGames;
---
+++ src/screens/collectionGames/styles.ts
@@ -0,0 +1,58 @@
+import { StyleSheet } from 'react-native';
+import { EdgeInsets } from 'react-native-safe-area-context';
+
+import { Elevation } from '@constants';
+import { globalStyles } from '@themes';
+import { getShadowStyle } from '@utility/style';
+
+const ThemedStyles = (theme: ThemeColors, insets: EdgeInsets) => {
+  return StyleSheet.create({
+    screen: {
+      ...globalStyles.flex1,
+      backgroundColor: theme.all.surfaceContainerLowest,
+    },
+    header: {
+      paddingTop: insets.top + 10,
+      flexDirection: 'row',
+      gap: 10,
+      padding: 10,
+      ...getShadowStyle(Elevation.level4),
+    },
+    searchContainer: {
+      flex: 1,
+      flexDirection: 'row',
+      alignItems: 'center',
+      backgroundColor: theme.all.surfaceContainerHighest,
+      padding: 10,
+      borderRadius: 10,
+      gap: 8,
+    },
+    listContentStyle: {
+      paddingTop: 10,
+      gap: 10,
+      position: 'relative',
+      paddingHorizontal: 10,
+      paddingBottom: insets.bottom + 10,
+    },
+    columnStyles: {
+      gap: 8,
+    },
+    listHeaderContainer: {
+      flex: 1,
+      flexDirection: 'row',
+      gap: 8,
+      marginTop: 5,
+      ...getShadowStyle(Elevation.level2),
+    },
+    listEmptyContainer: {
+      gap: 8,
+      flexDirection: 'column',
+    },
+    listEmptyRow: {
+      gap: 8,
+      flexDirection: 'row',
+    },
+  });
+};
+
+export default ThemedStyles;
