36769588521c995e0cee5a4d482fc6e611853f17
AkshatGKMit <akshat@gkmit.co>
2024-12-24 12:45:50 +0530

    feat: Add page list component

---
+++ src/components/pageListComponent/ListComponents.tsx
@@ -0,0 +1,87 @@
+import React, { memo } from 'react';
+import { View } from 'react-native';
+
+import Dropdown from '@components/dropdown';
+import { GameCardShimmer } from '@components/gameCard';
+import Loader from '@components/loader';
+import TextBlock from '@components/textBlock';
+import { Errors, orderByDropdownItems, systemPlatformDropdownItems } from '@constants';
+import { generateRandomString } from '@utility/helpers';
+
+export const ListEmptyComponent = memo(
+  ({ styles, theme, show = true }: GameListListEmptyComponentProps) => {
+    if (!show) {
+      return null;
+    }
+
+    return (
+      <View style={styles.listEmptyContainer}>
+        {Array.from({ length: 4 }).map((_, rowIndex) => {
+          return (
+            <View
+              key={generateRandomString(rowIndex) + rowIndex}
+              style={styles.listEmptyRow}
+            >
+              {Array.from({ length: 2 }).map((__, columnIndex) => (
+                <GameCardShimmer
+                  key={generateRandomString(columnIndex) + rowIndex + columnIndex}
+                  theme={theme}
+                />
+              ))}
+            </View>
+          );
+        })}
+      </View>
+    );
+  },
+);
+
+export const ListHeaderComponent = ({
+  styles,
+  orderBy,
+  selectedSystemPlatform,
+  setOrderBy,
+  setSelectedSystemPlatform,
+  show = true,
+}: GameListHeaderProps) => {
+  if (!show) {
+    return null;
+  }
+
+  return (
+    <View style={styles.listHeaderContainer}>
+      <Dropdown
+        items={orderByDropdownItems}
+        onSelect={setOrderBy}
+        value={orderBy}
+        buttonInitials="Order By"
+      />
+      <Dropdown
+        items={systemPlatformDropdownItems}
+        onSelect={setSelectedSystemPlatform}
+        value={selectedSystemPlatform}
+        buttonInitials="Platform"
+      />
+    </View>
+  );
+};
+
+export const ListFooterComponent = ({
+  hasData,
+  hasNextPage,
+  showNoConnectionScreenMessage,
+}: GameListFooterProps) => {
+  if (!hasData) {
+    return null;
+  }
+
+  if (showNoConnectionScreenMessage) {
+    return <TextBlock>{Errors.noInternet}</TextBlock>;
+  }
+
+  if (hasNextPage) {
+    return <Loader />;
+  }
+
+  return null;
+};
