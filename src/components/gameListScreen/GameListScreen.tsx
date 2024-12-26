660286357ec2a1da48dd30e8ffa3b0b3d19831a6
AkshatGKMit <akshat@gkmit.co>
2024-12-26 08:42:06 +0530

    fix: Fis show no games found

--- src/components/gameListScreen/GameListScreen.tsx
+++ src/components/gameListScreen/GameListScreen.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useMemo, useState } from 'react';
 import { FlatList, View } from 'react-native';
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
 
@@ -8,8 +8,15 @@ import {
   ListFooterComponent,
   ListHeaderComponent,
 } from '@components/pageListComponent';
+import TextBlock from '@components/textBlock';
 import useStyles from '@config/useStyles';
-import { orderByDropdownItems, QueryKeys, systemPlatformDropdownItems } from '@constants';
+import {
+  FontWeight,
+  orderByDropdownItems,
+  QueryKeys,
+  systemPlatformDropdownItems,
+  Typography,
+} from '@constants';
 import useGamesQuery from '@network/hooks/useGamesQuery';
 import { useAppSelector } from '@store';
 import { globalStyles } from '@themes';
@@ -42,7 +49,7 @@ const GameListScreen = ({
 
   const gameQueryKey: GameQueryKey = [mainGameListKey, url, gameQueryParams];
 
-  const { games, hasNextPage, fetchNextPage, isFetchingNextPage, online } =
+  const { games, isSuccess, hasNextPage, fetchNextPage, isFetchingNextPage, online } =
     useGamesQuery(gameQueryKey);
 
   const onEndReached = () => {
@@ -52,6 +59,8 @@ const GameListScreen = ({
   };
 
   const styles = useStyles((themeColors) => ThemedStyles(themeColors, insets));
+
+  const showNoResultsFound = useMemo(() => isSuccess && !games.length, [games.length, isSuccess]);
 
   return (
     <View style={styles.screen}>
@@ -64,41 +73,52 @@ const GameListScreen = ({
         styles={styles}
         listHeaderBackgroundColor={listHeaderBackgroundColor}
       />
-      <FlatList<Game>
-        numColumns={2}
-        style={globalStyles.flex1}
-        contentContainerStyle={styles.listContentStyle}
-        columnWrapperStyle={styles.columnStyles}
-        data={games}
-        renderItem={({ item }) => {
-          const { id } = item;
+      {showNoResultsFound ? (
+        <View style={[globalStyles.flex1, globalStyles.columnCenter]}>
+          <TextBlock
+            typography={Typography.titleLarge}
+            fontWeight={FontWeight.bold}
+          >
+            No games found!
+          </TextBlock>
+        </View>
+      ) : (
+        <FlatList<Game>
+          numColumns={2}
+          style={globalStyles.flex1}
+          contentContainerStyle={styles.listContentStyle}
+          columnWrapperStyle={styles.columnStyles}
+          data={games}
+          renderItem={({ item }) => {
+            const { id } = item;
 
-          return (
-            <GameCard
-              key={id}
-              game={item}
+            return (
+              <GameCard
+                key={id}
+                game={item}
+              />
+            );
+          }}
+          initialNumToRender={12}
+          keyExtractor={({ id }) => id.toString()}
+          onScroll={onScroll}
+          ListEmptyComponent={
+            <ListEmptyComponent
+              styles={styles}
+              theme={theme}
             />
-          );
-        }}
-        initialNumToRender={12}
-        keyExtractor={({ id }) => id.toString()}
-        onScroll={onScroll}
-        ListEmptyComponent={
-          <ListEmptyComponent
-            styles={styles}
-            theme={theme}
-          />
-        }
-        ListFooterComponent={
-          <ListFooterComponent
-            hasData={games.length !== 0}
-            hasNextPage={hasNextPage}
-            showNoConnectionScreenMessage={online.showNoConnectionScreenMessage}
-          />
-        }
-        showsVerticalScrollIndicator={false}
-        onEndReached={onEndReached}
-      />
+          }
+          ListFooterComponent={
+            <ListFooterComponent
+              hasData={games.length !== 0}
+              hasNextPage={hasNextPage}
+              showNoConnectionScreenMessage={online.showNoConnectionScreenMessage}
+            />
+          }
+          showsVerticalScrollIndicator={false}
+          onEndReached={onEndReached}
+        />
+      )}
     </View>
   );
 };
