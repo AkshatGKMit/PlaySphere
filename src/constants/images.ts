c8a72d33010c0825f75963d0b5f17c20eeae9375
AkshatGKMit <akshat@gkmit.co>
2025-01-07 11:43:50 +0530

    refactor: Compress Image

--- src/constants/images.ts
+++ src/constants/images.ts
@@ -1,7 +1,7 @@
 export const IMAGES = {
-  APP_LOGO: require('@assets/images/appLogo.jpeg'),
+  APP_LOGO: require('@assets/images/appLogo.png'),
   PHOTO_COLLAGE: require('@assets/images/photoCollage.png'),
-  GAME_COVER: require('@assets/images/gameCover.jpg'),
-  COLLECTION_COVER: require('@assets/images/collectionCover.jpg'),
-  PROFILE_COVER: require('@assets/images/profile.jpg'),
+  GAME_COVER: require('@assets/images/gameCover.png'),
+  COLLECTION_COVER: require('@assets/images/collectionCover.png'),
+  PROFILE_COVER: require('@assets/images/profile.png'),
 };
